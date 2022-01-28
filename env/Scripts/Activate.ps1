c42e1ddeea0f4025ede1ee48b1441834165eccb5
Leffatwissam <96617582+Leffatwissam@users.noreply.github.com>
2022-01-28 17:50:23 +0100

    last

--- env/Scripts/Activate.ps1
+++ env/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\DELL\Desktop\WIS\GestionStageV2\env"
+$env:VIRTUAL_ENV="C:\Users\DELL\Desktop\proj\GestionStageV2\env"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- env/Scripts/django-admin.py
+++ env/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\dell\desktop\wis\gestionstagev2\env\scripts\python.exe
+#!c:\users\dell\desktop\proj\gestionstagev2\env\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
